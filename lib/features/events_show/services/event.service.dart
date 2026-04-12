ceb4f0928a635d83d006fb9729e1196452bbcee4
natebps <natebpss@gmail.com>
2026-04-12 23:51:47 +0700

    new: event, animal GET + add data into zone, animal, event

---
+++ lib/features/events_show/services/event.service.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/event_model.dart';
+
+class EventService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  // GET all events
+  Future<List<EventModel>> getEvents() async {
+    try {
+      final snapshot = await _db.collection('event').get();
+      return snapshot.docs
+          .map((doc) => EventModel.fromMap(doc.id, doc.data()))
+          .toList();
+    } catch (e) {
+      print('Error getting events: $e');
+      return [];
+    }
+  }
+}
