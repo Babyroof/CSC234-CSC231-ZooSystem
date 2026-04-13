b3d7ed91d054ee6863341bec656fc26acb45f6f7
pornchanok <pornchanok.phua@mail.kmutt.ac.th>
2026-04-13 16:34:07 +0700

    update: add navbar navigate

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:zoopernova_zoo_system/core/widgets/zoo_bottom_nav.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-   }
+    return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: ZooBottomNav(currentIndex: 0),
+    );
+  }
 }
