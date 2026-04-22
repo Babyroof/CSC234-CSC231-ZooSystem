36bda5c2a3766d9c07e8088fb05541d321ec8583
natebps <natebpss@gmail.com>
2026-04-23 00:06:10 +0700

    Integrate Home

--- lib/features/animals_info/services/animal_service.dart
+++ lib/features/animals_info/services/animal_service.dart
@@ -5,6 +5,27 @@ import '../models/zone_model.dart';
 
 class AnimalService {
   FirebaseFirestore get _db => FirebaseFirestore.instance;
+
+  //Random Animals
+  Future<List<AnimalModel>> getRandomPopularAnimals(int count) async {
+  try {
+    final snap = await _db.collection('animal').get();
+    print('Firestore animal docs count: ${snap.docs.length}'); // เพิ่มตรงนี้
+    
+    List<AnimalModel> list = snap.docs
+        .map((doc) {
+          print('Doc ID: ${doc.id}, Data: ${doc.data()}'); // ดู raw data
+          return AnimalModel.fromMap(doc.id, doc.data());
+        })
+        .toList();
+    list.shuffle();
+    return list.take(count).toList();
+  } catch (e) {
+    print('getRandomPopularAnimals ERROR: $e'); // ดู error จริงๆ
+    return [];
+  }
+}
+
   // GET all zones
   Future<List<ZoneModel>> getZones() async {
     try {
